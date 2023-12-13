6546219f2c5e0de31cc787be8ed4a2d7b85168cb
Roberto Morrinson <19396771+morrinson11@users.noreply.github.com>
2023-12-13 01:15:23 -0500

    primeras configuraciones del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GiftsModule } from './gifs/gifts.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GiftsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
