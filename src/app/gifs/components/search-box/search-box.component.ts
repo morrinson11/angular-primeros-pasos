cd4e571527b8baaaf068744110b680b38a4c3689
Roberto Morrinson <19396771+morrinson11@users.noreply.github.com>
2023-12-14 20:14:22 -0500

    clase de viewchild

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,25 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template:`
+    <h5>Buscar:</h5>
+    <input type="text"
+    class="form-control"
+    placeholder="Buscar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput>
+  `
+})
+
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  //searchTag(newTag:string){
+    searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+  }
+}
--- src/app/gifs/gifts.module.ts
+++ src/app/gifs/gifts.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomePageComponent } from './pages/homepage/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
   ],
   exports:[
-
+    HomePageComponent
   ]
 })
 export class GiftsModule { }
